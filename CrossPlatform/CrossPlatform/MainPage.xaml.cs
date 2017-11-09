21234841a7aa30570828f1e93c9ef8a09e3b5422
Viktor <vice.eu@gmail.com>
2017-11-09 02:16:07 +0100

    Add project files.

---
+++ CrossPlatform/CrossPlatform/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace CrossPlatform
+{
+
+ 
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            videoview.FullScreen = true;
+           // videoview.SetStartAction(OnAppearing);
+
+        }
+
+
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            
+        }
+
+    }
+
+    
+}
